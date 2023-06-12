fa17cdce618a8dc0c71bfaf3efc72d9e48a986cc
torcuata22 <marilynjmarquez@gmail.com>
2023-06-12 14:11:03 -0400

    ran migrations

--- db/migrate/20230612180231_create_lists.rb
+++ db/migrate/20230612180231_create_lists.rb
@@ -2,7 +2,7 @@ class CreateLists < ActiveRecord::Migration[7.0]
   def change
     create_table :lists do |t|
       t.string :description
-      t.boolean :completed, default :false
+      t.boolean :completed, default: false
 
       t.timestamps
     end
