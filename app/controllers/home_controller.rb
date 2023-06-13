0ae2d23af24cd182a6a7e138dc7ed6890fadce72
torcuata22 <marilynjmarquez@gmail.com>
2023-06-13 17:05:08 -0400

    new controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+
+    def trash_em
+        
+    end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :lists
   root 'lists#index'
+  get 'home/trash_em'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
