84d81f6720d1a6ee0dae29d72646f08814f5f373
torcuata22 <marilynjmarquez@gmail.com>
2023-06-16 14:48:43 -0400

    checked

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
 
     def trash_em
-        
+        @trash_em=List.where(:completed => true).destroy_all
+        redirect_to lists_url, notice: "All marked items were successfully deleted." 
+    end
+
+    def trash_em_all
+        @trash_em_all=List.all.destroy_all
+        redirect_to lists_url, notice: "All items were successfully deleted." 
     end
 
 
